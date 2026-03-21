a5ab8b4c5fa862b0c9dfd2bb393586130572357b
wendelmjesus <wendelmjesus@outlook.comn>
2026-03-21 02:57:50 -0300

    add: product filters and extra product cards on products page

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 const slides = document.querySelectorAll(".slides img");
 const indicators = document.querySelectorAll(".indicators span");
+const filter = document.querySelector("#filter");
+const products = document.querySelectorAll(".product-cards");
 
+// FILTRO DE PRODUTOS
+filter.addEventListener("change", () => {
+  const selectedCategory = filter.value;
+
+  products.forEach(product => {
+    const productCategory = product.dataset.category;
+
+    if (selectedCategory === "all") {
+      product.style.display = "block";
+    } else if (productCategory === selectedCategory) {
+      product.style.display = "block";
+    } else {
+      product.style.display = "none";
+    }
+  });
+});
+
+// SLIDER
 let index = 0;
-
 setInterval(() => {
   slides[index].classList.remove("active");
   indicators[index].classList.remove("active");
@@ -13,6 +32,7 @@ setInterval(() => {
   indicators[index].classList.add("active");
 }, 5000);
 
+// SCROLL HEADER
 window.addEventListener("scroll", () => {
   const header = document.querySelector("header");
 
